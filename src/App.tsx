44e2d29a2b8672020136ebcb309daa9380682530
Himanshu Kataria <katariahk03@gmail.com>
2022-08-06 13:49:31 +0530

    Header add

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.scss';
+import Header from './Header/Header';
+const App = () => {
+  return (
+    <div>
+        <Header/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.scss';
+const Header = () => {
+  return (
+    <header className='header'>
+        <span className="header__title">
+            Random Quote
+        </span>
+    </header>
+  )
+}
+
+export default Header
